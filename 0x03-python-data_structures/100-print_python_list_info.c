1aaf8882bda848f4e59defd772f886a0fb7fe1a6
Fekaduk <fekadu9985@gmail.com>
2024-01-09 10:23:51 -0800

    updte advanced

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -1,6 +1,6 @@
-#include "/usr/include/python3.4/Python.h"
-#include "/usr/include/python3.4/listobject.h"
-#include "/usr/include/python3.4/object.h"
+#include "/usr/include/python3/Python.h"
+#include "/usr/include/python3/listobject.h"
+#include "/usr/include/python3/object.h"
 #include <stdio.h>
 
 void print_python_list_info(PyObject *p)
